7ac754348c4ac6b2437d6fb6fedec4fc91a7d266
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-14 12:54:49 +0100

    styling of the home page

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -6,12 +6,14 @@ const Article = ({ articles }) => {
 
   return (
     articles.map(a =>
+      <div className="container-wrapper">
       <div className="article-card-container" key={a.id}>
       <div className="title-container">
-      <div>{a.webTitle}</div>
+      <div className="title">{a.webTitle}</div>
       </div>
       <div className="img-container">
       <img src={a.fields.thumbnail}></img>
+      </div>
       </div>
       </div>
       )
