8dab2569173ef69731797ca104328876feef71b3
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-14 12:17:59 +0100

    unique key bug fix and image added to card

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useState } from 'react';
+import './Article.css';
 
 const Article = ({ articles }) => {
 
   return (
     articles.map(a =>
-      <div>
+      <div className="article-card-container" key={a.id}>
+      <div className="title-container">
       <div>{a.webTitle}</div>
+      </div>
+      <div className="img-container">
+      <img src={a.fields.thumbnail}></img>
+      </div>
       </div>
       )
     
